4ca0cc1ee3c8371374f9be320be136bb7d47cabc
Ilya <kuskovig@gmail.com>
2021-08-16 22:46:05 +0700

    fix

--- tests/test_opencart_main.py
+++ tests/test_opencart_main.py
@@ -5,6 +5,15 @@ from selenium.common.exceptions import TimeoutException
 import random
 
 
+def wait_for_element(browser, element):
+    try:
+        element = WebDriverWait(browser, 2). \
+            until(EC.presence_of_element_located(element))
+    except TimeoutException:
+        raise AssertionError("Couldn't find element after 2 seconds")
+    return element
+
+
 def test_opens_mainpage_with_default_url(browser, url):
     browser.get(url)
     assert browser.title == "Your Store", "Wrong title of the  main page"
@@ -12,11 +21,8 @@ def test_opens_mainpage_with_default_url(browser, url):
 
 def test_there_is_slideshow(browser, url):
     browser.get(url)
-    try:
-        WebDriverWait(browser, 2). \
-            until(EC.presence_of_element_located(OpencartMainPageLocators.MAIN_PAGE_SLIDESHOW))
-    except TimeoutException:
-        raise AssertionError("Couldn't find element after ")
+    wait_for_element(browser, OpencartMainPageLocators.MAIN_PAGE_SLIDESHOW)
+
 
 
 def test_alert_correctly_closes(browser, url):
@@ -27,11 +33,7 @@ def test_alert_correctly_closes(browser, url):
                                             RECOMMENDED_ITEMS_WISHLIST_BUTTONS)[random_wishlist]
     wishlist_button.click()
     # waiting for alert to appear and closing it
-    try:
-        alert_close_button = WebDriverWait(browser, 2). \
-            until(EC.presence_of_element_located(OpencartMainPageLocators.WISHLIST_ATTEMPT_ALERT_CLOSE))
-    except TimeoutException:
-        raise AssertionError("Couldn't find element after 2 seconds")
+    alert_close_button = wait_for_element(browser, OpencartMainPageLocators.WISHLIST_ATTEMPT_ALERT_CLOSE)
     alert_close_button.click()
     # asserting that alert correctly closed
     try:
@@ -43,26 +45,14 @@ def test_alert_correctly_closes(browser, url):
 
 def test_presence_of_cart_button(browser, url):
     browser.get(url)
-    try:
-        WebDriverWait(browser, 2). \
-            until(EC.presence_of_element_located(OpencartMainPageLocators.MAIN_PAGE_CART_BUTTON))
-    except TimeoutException:
-        raise AssertionError("Couldn't find element after 2 seconds")
+    wait_for_element(browser, OpencartMainPageLocators.MAIN_PAGE_CART_BUTTON)
 
 
 def test_presence_of_currency_selector(browser, url):
     browser.get(url)
-    try:
-        WebDriverWait(browser, 2). \
-            until(EC.presence_of_element_located(OpencartMainPageLocators.MAIN_PAGE_CURRENCY_SELECTOR))
-    except TimeoutException:
-        raise AssertionError("Couldn't find element after 2 seconds")
+    wait_for_element(browser, OpencartMainPageLocators.MAIN_PAGE_CURRENCY_SELECTOR)
 
 
 def test_presence_of_search_button(browser, url):
     browser.get(url)
-    try:
-        WebDriverWait(browser, 2). \
-            until(EC.presence_of_element_located(OpencartMainPageLocators.SEARCH_BAR_SEARCH_BUTTON))
-    except TimeoutException:
-        raise AssertionError("Couldn't find element after 2 seconds")
+    wait_for_element(browser, OpencartMainPageLocators.SEARCH_BAR_SEARCH_BUTTON)
